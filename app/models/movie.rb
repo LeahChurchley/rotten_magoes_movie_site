bec1d8f1987d6dd2c93643fd93ab5cce01bb6684
Leah Churchley <leah.churchley@gmail.com>
2016-04-27 00:31:05 +0000

    added review model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,31 +1,33 @@
 class Movie < ActiveRecord::Base
 
-      validates :title,
-        presence: true
+  has_many :reviews
 
-      validates :director,
-        presence: true
+  validates :title,
+    presence: true
 
-      validates :runtime_in_minutes,
-        numericality: { only_integer: true }
+  validates :director,
+    presence: true
 
-      validates :description,
-        presence: true
+  validates :runtime_in_minutes,
+    numericality: { only_integer: true }
 
-      validates :poster_image_url,
-        presence: true
+  validates :description,
+    presence: true
 
-      validates :release_date,
-        presence: true
+  validates :poster_image_url,
+    presence: true
 
-      validate :release_date_is_in_the_past
+  validates :release_date,
+    presence: true
 
-      protected
+  validate :release_date_is_in_the_past
 
-      def release_date_is_in_the_past
-        if release_date.present?
-          errors.add(:release_date, "should be in the past") if release_date > Date.today
-        end
-      end
+  protected
 
+  def release_date_is_in_the_past
+    if release_date.present?
+      errors.add(:release_date, "should be in the past") if release_date > Date.today
     end
+  end
+
+end
